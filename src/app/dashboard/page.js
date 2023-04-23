6520d4f23910821a1a5793392c5bb4a2e841d29c
Code Simple <zabardast@gmail.com>
2023-04-23 14:15:56 +0500

    commit

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,19 +1,42 @@
 "use client";
-const api = "https://jsonplaceholder.typicode.com/users";
 
-const loadDataFromServer = async () => {
-  const response = await fetch(api, { cache: "no-store" }); // force fetching is by default but you can ommit it by no-store
-  return response.json();
-};
+import useSWR from "swr";
 
-export default async function Dashboard() {
-  const users = await loadDataFromServer();
+const fetcher = (path) =>
+  fetch(`https://jsonplaceholder.typicode.com${path}`).then((res) =>
+    res.json()
+  );
+
+export default function Dashboard() {
+  const users = useSWR("/users", fetcher);
+  console.log(users);
+  if (users.isLoading) return <h1>Loading...</h1>;
   return (
     <>
       <h1>My blog</h1>
-      {users.map((user) => (
+      {users.data?.map((user) => (
         <h1 key={user.id}>{user.name}</h1>
       ))}
     </>
   );
 }
+
+// Alternative Method : Recommended by NextJs13 Docs
+// const api = "https://jsonplaceholder.typicode.com/users";
+
+// const loadDataFromServer = async () => {
+//   const response = await fetch(api, { cache: "no-store" }); // force fetching is by default but you can ommit it by no-store
+//   return response.json();
+// };
+
+// export default async function Dashboard() {
+//   const users = await loadDataFromServer();
+//   return (
+//     <>
+//       <h1>My blog</h1>
+//       {users.map((user) => (
+//         <h1 key={user.id}>{user.name}</h1>
+//       ))}
+//     </>
+//   );
+// }
